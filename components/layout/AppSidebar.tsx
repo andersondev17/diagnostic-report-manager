a3a1d65ef8bae2e7affbd9297bddd308a3cc5a36
Anderson Lopez <52222621+TheGodFatherpte@users.noreply.github.com>
2026-01-25 18:27:01 -0500

    feature: Implement sidebar

---
+++ components/layout/AppSidebar.tsx
@@ -0,0 +1,52 @@
+"use client"
+
+
+import * as React from "react"
+
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarFooter,
+  SidebarHeader,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+} from '@/components/ui/sidebar'
+import { NavDocuments } from '@/features/navbar/components/NavDocuments'
+import { NavMain } from '@/features/navbar/components/NavMain'
+import { NavSecondary } from '@/features/navbar/components/NavSecondary'
+import { NavUser } from '@/features/navbar/components/NavUser'
+import { data } from "@/lib/db/mock-data"
+import { IconInnerShadowTop } from "@tabler/icons-react"
+
+
+
+export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
+  return (
+    <Sidebar collapsible="offcanvas" {...props}>
+      <SidebarHeader>
+        <SidebarMenu>
+          <SidebarMenuItem>
+            <SidebarMenuButton
+              asChild
+              className="data-[slot=sidebar-menu-button]:!p-1.5"
+            >
+              <a href="/">
+                <IconInnerShadowTop className="!size-5" />
+                <span className="text-base font-semibold">DALOG.</span>
+              </a>
+            </SidebarMenuButton>
+          </SidebarMenuItem>
+        </SidebarMenu>
+      </SidebarHeader>
+      <SidebarContent>
+        <NavMain items={data.navMain} />
+        <NavDocuments items={data.documents} />
+        <NavSecondary items={data.navSecondary} className="mt-auto" />
+      </SidebarContent>
+      <SidebarFooter>
+        <NavUser user={data.user} />
+      </SidebarFooter>
+    </Sidebar>
+  )
+}
